949aab08820c0077f869bbdfd152ec675b9e4897
admin <gamr@ip-172-31-9-28.us-west-1.compute.internal>
2015-01-10 07:52:04 +0000

    adduser implemented

--- gamr_server/gamr/serverProxy.py
+++ gamr_server/gamr/serverProxy.py
@@ -3,8 +3,23 @@ import psycopg2, json
 
 app = Flask(__name__)
 
-#def adduser(username, password, description, games, platforms, genres, seriousness, reputation, miscQuals):
-	
+def adduser(username, password, description, games, platforms, genres, seriousness, reputation, miscQuals):
+	conn = psycopg2.connect("dbname=gamr user=gamr")
+	cur = conn.cursor()
+	cur.execute("INSERT INTO users (username, password, description, games, platforms, genres, seriousness, reputation, miscQuals) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (username, password, description, games, platforms, genres, seriousness, reputation, miscQuals))
+	conn.commit()
+	#print str(cur.fetchone())
+
+def getGetUserInfo(user):
+	conn = psycopg2.connect("dbname=gamr user=gamr")
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM users WHERE username = '" + user + "';");
+	ret = cur.fetchone();
+	if (ret != None):
+		retjson = '{ "username":"' + ret[0] + '", "description":"' + ret[2] + '", "games":' + ret[3] + ', "platforms":' + ret[4] + ', "genres":' + ret[5] + ', "seriousness":' + str(ret[6]) + ', "reputation":' + str(ret[7]) + ', "miscQuals":' + ret[8] + ' }'
+	else:
+		retjson = '{"error":"404", "description":"Not Found: The specified user does not exist"}'
+	return retjson
 
 @app.route("/")
 def root():
@@ -17,9 +32,29 @@ def getUserInfo(user):
 	if request.method == "GET":
 		cur.execute("SELECT * FROM users WHERE username = '" + user + "';");
 		ret = cur.fetchone();
-		return json.dumps(ret);
+		retjson = '{ "username":"' + ret[0] + '", "description":"' + ret[2] + '", "games":' + ret[3] + ', "platforms":' + ret[4] + ', "genres":' + ret[5] + ', "seriousness":' + str(ret[6]) + ', "reputation":' + str(ret[7]) + ', "miscQuals":' + ret[8] + ' }'
+		return retjson
+	elif request.method == "POST":
+		cur.execute("SELECT * FROM users WHERE username = '" + user + "';");
+		ret = cur.fetchone();
+		if (ret != None and ret[1] == request.form['password']):
+			retjson = '{ "username":"' + ret[0] + '", "description":"' + ret[2] + '", "games":' + ret[3] + ', "platforms":' + ret[4] + ', "genres":' + ret[5] + ', "seriousness":' + str(ret[6]) + ', "reputation":' + str(ret[7]) + ', "miscQuals":' + ret[8] + ' }'
+			#print retjson
+			return retjson
+		else:
+			#print errortxt
+			return '{"error":"404", "description":"Not Found: The specified username and password combination are invalid."}'
+
+@app.route("/add/user/<user>", methods=['POST'])
+def newUser(user):
+	if (json.loads(getGetUserInfo(user))["error"] == "404"):
+		adduser(user, request.form['password'], request.form['description'], request.form['games'], request.form['platforms'], request.form['genres'], request.form['seriousness'], request.form['reputation'], request.form['miscQuals'])
+		return getGetUserInfo(user);
+	else:
+		return '{"error":"420", "description":"Method Failure: The specified username already exists."}'
+
 
 #print(getUserInfo("BoneZ"))
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0")
+	app.run(host="0.0.0.0", debug=True)
